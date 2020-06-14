c055414c51c3701b48df401a245056513f606fc6
nathan-m-schneider-22 <nathan.m.schneider.22@dartmouth.edu>
2020-06-13 19:32:11 -0600

    change to lcc

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require("dotenv").config()
 const port = process.env.PORT || 9090
 var schedule = require('node-schedule');
 var request = require("request");
+const puppeteer = require('puppeteer');
 
 
 app.use(bodyParser.json())
@@ -61,9 +62,8 @@ var j = schedule.scheduleJob('*/5 * * * *', function(){
           var dbo = db.db(DB);
           for (i=0;i<data.length;i++){data[i].time = time}
 
-          dbo.collection("scraped").insertMany(data, function(err, res) {
+          dbo.collection("scraped").insertOne(data, function(err, res) {
             if (err) throw err;
-            console.log(data[0])
             console.log("documents inserted");
             db.close();
           });
@@ -74,7 +74,7 @@ var j = schedule.scheduleJob('*/5 * * * *', function(){
     request('https://laundry-uptime.herokuapp.com/', { json: true }, (err, res, body) => {
       if (err) { return console.log(err); }
       console.log("Self-call")
-      console.log(body);
+      // console.log(body);
 
     });
 
@@ -88,43 +88,19 @@ function scrapeData(){
     console.log("Scraping data")
     const https = require('https');
     var p1 = new Promise((resolve, reject) => {
-
-      // or
-      // reject(new Error("Error!"));
-      
-    
-    request("https://m.dartmouth.edu/laundryview/index/", function (error, response, body) {
+    const url = "https://proxybot.io/api/v1/JF33bQx0Xeb8ZHdr8fgovyZ82d13?render_js=true&url=https://www.peoplecount.live/embed/TqbMydvq_/oKbsHvaFb/__master__"
+    request(url, function (error, response, body) {
         if (!error) {
-            var ar = []
-            data ={}
-            var parser = new htmlparser.Parser({
-
-                ontext: function(text){
-
-                  ar.push(text)
-                }
-              }, {decodeEntities: true});
-              parser.write(body);
-              parser.end();
-              var i =0
-              valid_times = []
-
-              while (i<ar.length ){
-
-                if (ar[i].length < 100 && ar[i].includes("available")) {
-                    valid_times.push({
-                        "building":ar[i-1],
-                        "washers":ar[i].substring(0,ar[i].length-17),
-                        "dryers":ar[i+1].substring(0,ar[i+1].length-16)
-                    })
-                    i+=2
-                }
-                i++;
-              }
-
-              
-
-              resolve( valid_times)
+            data ={
+              time:Date.now()
+            }
+            i = body.indexOf("count regular")
+            // console.log(body.slice(i+15,i+17))
+            count = parseInt(body.slice(i+15,i+17).replace("<",""))
+            // console.log(count)
+            data.count = count
+            console.log(data)
+            resolve(data)
         } else {
         console.log(error);
         }
@@ -135,6 +111,7 @@ function scrapeData(){
 
 }
 
+scrapeData()
 
 /**
  * Simple Error Handling
@@ -146,6 +123,6 @@ app.use(function(err, req, res, next){
 /**
  * Port listening, on localhost
  */
-app.listen(port, () => console.log(`API Server listening on port ${port}!`))
+// app.listen(port, () => console.log(`API Server listening on port ${port}!`))
 
 
